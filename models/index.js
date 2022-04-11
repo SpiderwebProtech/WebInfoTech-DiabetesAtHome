c1e20ecb614cead85f1b25898623ce1fb7e371ba
unknown <hutchinsp01@gmail.com>
2022-04-11 23:32:35 +1000

    Test: Testing pre-commit

--- models/index.js
+++ models/index.js
@@ -1,21 +1,22 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'demo',
-})
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "demo",
+});
+
 // Exit on error
-const db = mongoose.connection.on('error', (err) => {
-    console.error(err)
-    process.exit(1)
-})
+const db = mongoose.connection.on("error", (err) => {
+  console.error(err);
+  process.exit(1);
+});
 
-db.once('open', async () => {
-    console.log(`Mongo connection started on ${db.host}:${db.port}`)
-})
+db.once("open", async () => {
+  console.log(`Mongo connection started on ${db.host}:${db.port}`);
+});
 
-require('./patientModel')
+require("./patientModel");
