16e75ac5c2f5a6c6ba71d3179aa0f1e94d99b0d6
unknown <hutchinsp01@gmail.com>
2022-04-12 23:43:23 +1000

    Fix: Moved '/:id' to the bottom of routes so it doesnt match till the end

--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -6,7 +6,6 @@ const patientController = require("../controllers/patientController");
 
 patientRouter.get("/", patientController.getPatientLogin);
 patientRouter.post("/login", patientController.postPatientLogin);
-patientRouter.get("/:id", patientController.getPatientById);
 
 patientRouter.get("/about-diabetes", (req, res) => {
   res.render("patient/about-diabetes");
@@ -15,5 +14,7 @@ patientRouter.get("/about-diabetes", (req, res) => {
 patientRouter.get("/about-this-website", (req, res) => {
   res.render("patient/about-this-website");
 });
+
+patientRouter.get("/:id", patientController.getPatientById);
 
 module.exports = patientRouter;
