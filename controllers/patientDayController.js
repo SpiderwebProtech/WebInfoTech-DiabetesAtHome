23aabffe56211158c2026d307309e2786e74c0e7
unknown <hutchinsp01@gmail.com>
2022-04-25 15:04:14 +1000

    Feat: Start formatting patient histroy for clinician

--- controllers/patientDayController.js
+++ controllers/patientDayController.js
@@ -1,6 +1,7 @@
 const dateFunctions = require("../public/javascript/dateFunctions");
 
 const PatientDay = require("../models/patientDayModel");
+const Patient = require("../models/patientModel");
 
 const getPatientDayByPatientIdToday = async (id) => {
   const today = dateFunctions.getMelbourneDate();
@@ -18,4 +19,45 @@ const getPatientDayByPatientIdToday = async (id) => {
   }
 };
 
-module.exports = { getPatientDayByPatientIdToday };
+const getPatientDayByPatientIdTodayDropId = async (id) => {
+  const today = dateFunctions.getMelbourneDate();
+  try {
+    const patientDay = await PatientDay.findOne(
+      {
+        patient: id,
+        date: today,
+      },
+      { _id: 0 }
+    ).lean();
+    if (!patientDay) {
+      return null;
+    }
+    return patientDay;
+  } catch (err) {
+    return null;
+  }
+};
+
+const getPatientHistoryById = async (id) => {
+  const date = dateFunctions.getMelbourneDate();
+  try {
+    const patientHistory = await PatientDay.find({ patient: id })
+      .sort({ date: -1 })
+      .lean();
+    if (!patientHistory) {
+      return null;
+    }
+    // console.log(patientHistory.slice(-1)[0].date)
+    // const dateArray = dateFunctions.getDates(dateFunctions.getProperDate(patientHistory.slice(-1)[0].date), dateFunctions.getProperDate(date))
+    // console.log(dateArray)
+    return patientHistory;
+  } catch (err) {
+    return null;
+  }
+};
+
+module.exports = {
+  getPatientDayByPatientIdToday,
+  getPatientDayByPatientIdTodayDropId,
+  getPatientHistoryById,
+};
--- public/javascript/dateFunctions.js
+++ public/javascript/dateFunctions.js
@@ -12,4 +12,34 @@ const getMelbourneTime = () => {
   return time;
 };
 
-module.exports = { getMelbourneDate, getMelbourneTime };
+// const getProperDate = (date) => {
+//   //  Convert a "dd/MM/yyyy" string into a Date object
+//   let d = dateString.split("/");
+//   let dat = new Date(d[2] + '/' + d[1] + '/' + d[0]);
+//   return dat;
+// }
+
+// // Taken from https://stackoverflow.com/questions/4413590/javascript-get-array-of-dates-between-2-dates
+// Date.prototype.addDays = function(days) {
+//   var date = new Date(this.valueOf());
+//   date.setDate(date.getDate() + days);
+//   return date;
+// }
+
+// function getDates(startDate, stopDate) {
+//   var dateArray = new Array();
+//   var currentDate = startDate;
+//   while (currentDate <= stopDate) {
+//       dateArray.push(new Date (currentDate));
+//       currentDate = currentDate.addDays(1);
+//   }
+//   return dateArray;
+// }
+// /********************************************************************************* */
+
+module.exports = {
+  getMelbourneDate,
+  getMelbourneTime,
+  getDates,
+  getProperDate,
+};
